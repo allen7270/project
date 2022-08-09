2b073cc1afab6b012f60b203b7bb2f820a13f7d6
allen7270 <haha30932@gmail.com>
2022-08-09 18:13:35 +0800

    member alter ok

---
+++ src/main/java/Alter.java
@@ -0,0 +1,48 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import project.Data;
+import project.DataBase;
+
+/**
+ * Servlet implementation class Alter
+ */
+@WebServlet("/Alter")
+public class Alter extends HttpServlet {
+	ServletConfig scg; 
+	public void init(ServletConfig scg) throws ServletException {
+		super.init(scg);
+		this.scg= scg;
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		var context= scg.getServletContext();
+		String account= (String)context.getAttribute("account");
+		String passwd= (String)context.getAttribute("passwd");
+		String passwdAlter= request.getParameter("passwd");
+		String mail= request.getParameter("mail");
+		String phone= request.getParameter("phone");
+		var database= new DataBase();
+		var data= new Data();
+		data.setAccount(account);
+		data.setPasswd(passwd);
+		data.setMail(mail);
+		data.setphone(phone);
+		database.alter(data,passwdAlter);
+		response.sendRedirect("Login.html");
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
---
+++ src/main/webapp/js/alter.js
@@ -0,0 +1,64 @@
+function show(){
+    if(window.ActiveXObject){
+    xmlHTTP=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    else if(window.XMLHttpRequest){
+        xmlHTTP=new XMLHttpRequest();
+    }
+    xmlHTTP.open("get","http://localhost:8080/project/Member?", true);
+    xmlHTTP.onreadystatechange=function check_status(){
+    if(xmlHTTP.readyState == 4 && xmlHTTP.status == 200){
+        var ans=xmlHTTP.responseText;
+        ans=ans.split(',');
+        document.getElementById("username").innerText= ans[0];
+        document.getElementById("account").innerText= ans[1];
+    }
+    }
+    xmlHTTP.send();
+}
+
+function check(){
+    passwd= document.getElementById('passwd');
+    mail= document.getElementById('mail');
+    phone= document.getElementById('phone');
+    mailpattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    phonepattern= /09\d{8}/;
+    go= true;
+    // passwd
+    if(passwd.value==""){
+        alert('密碼未填寫!');
+        passwd.focus();
+        go= false;
+        return false;
+    } else if(passwd.value.length<5){
+        alert('密碼長度不足!');
+        passwd.select();
+        go= false;
+        return false;
+    } else{
+        go= true;
+    }
+    // mail
+    if(!mailpattern.test(mail.value)){
+        alert('信箱格式錯誤!');
+        mail.focus();
+        go= false;
+        return false;
+    } else{
+        go= true;
+    }
+    // phone
+    if(!phonepattern.test(phone.value)){
+        alert('電話未填寫!');
+        phone.focus();
+        go= false;
+        return false;
+    } else{
+        go= true;
+    }
+    // submit!!
+    if(go){
+        alert("修改成功!");
+        document.dataForm.submit();
+    }
+}
