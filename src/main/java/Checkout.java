92166ba7cbe58ad7f605481f03182d509a1cd26d
allen7270 <haha30932@gmail.com>
2022-08-12 13:57:41 +0800

    OrderInfo

---
+++ src/main/java/Checkout.java
@@ -0,0 +1,59 @@
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import project.DataBase;
+import project.OrderData;
+/**
+ * 
+ * @projectName project
+ * @className Checkout
+ * @author allen
+ * @date 2022年8月12日 上午12:00:36
+ */
+@WebServlet("/Checkout")
+public class Checkout extends HttpServlet {
+	
+	ServletConfig cfg;
+	public void init(ServletConfig cfg) throws ServletException {
+		super.init(cfg);
+		this.cfg= cfg;
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8");
+		var context= cfg.getServletContext();
+		// orderDetail
+		var order= new ArrayList<String>();
+		String[] data= {"set1","set2","set3","carte1","carte2","carte3","carte4","carte5","carte6","carte7","carte8"};
+		for(int i=0; i<data.length; i++) {
+			order.add((String) context.getAttribute(data[i]));
+			if(order.get(i).equals("")) {
+				order.set(i, "0");
+			}
+		}
+		// orDerInfo
+		String username= request.getParameter("username");
+		String loc= request.getParameter("loc");
+		String mail= request.getParameter("mail");
+		String phone= request.getParameter("phone");
+		String pay= request.getParameter("payMethod");
+		var orderData= new OrderData(username, pay, loc, mail, phone);
+		var dataBase= new DataBase();
+		dataBase.order(order, orderData);
+		// back to Menu.html
+		response.sendRedirect("Menu.html");
+	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
