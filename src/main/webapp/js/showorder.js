168baa9edac21cd4b4ff2b0bd3b964a35135e8c8
allen7270 <haha30932@gmail.com>
2022-08-13 15:17:14 +0800

    修改版面

--- src/main/webapp/js/showorder.js
+++ src/main/webapp/js/showorder.js
@@ -1,20 +1,27 @@
 function show(){
-	if(window.ActiveXObject){
-		xmlHTTP=new ActiveXObject("Microsoft.XMLHTTP");
-		}
-		else if(window.XMLHttpRequest){
-			xmlHTTP=new XMLHttpRequest();
-		}
-		xmlHTTP.open("get","http://localhost:8080/project/Showorder?", true);
-		xmlHTTP.onreadystatechange=function check_status(){
-		if(xmlHTTP.readyState == 4 && xmlHTTP.status == 200){
-			var ans=xmlHTTP.responseText;
-			ans=ans.split(',');
-            document.write("ans:"+ans[1]);
-            // document.getElementById("tdName").innerText= ans[0];
-            // document.getElementById("tdDate").innerText= ans[1];
-            // document.getElementById("tdItem").innerText= ans[2];
-		}
-		}
-		xmlHTTP.send();
+    if(window.ActiveXObject){
+    xmlHTTP=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    else if(window.XMLHttpRequest){
+        xmlHTTP=new XMLHttpRequest();
+    }
+    xmlHTTP.open("post","http://localhost:8080/project/Showorder?", true);
+    xmlHTTP.onreadystatechange=function check_status(){
+    if(xmlHTTP.readyState == 4 && xmlHTTP.status == 200){
+        var ans=xmlHTTP.responseText;
+        var item="";
+        ans=ans.split(',');
+        document.getElementById("tdName").innerText= ans[0];
+        document.getElementById("tdDate").innerText= ans[1];
+        document.getElementById("tdLoc").innerText= ans[2];
+        document.getElementById("tdMail").innerText= ans[3];
+        document.getElementById("tdCell").innerText= ans[4];
+        document.getElementById("tdPay").innerText= ans[5];
+        for(i=6; i<ans.length; i++){
+            item+=ans[i];
+        }
+        document.getElementById("tdItem").innerText= item;
+    }
+    }
+    xmlHTTP.send();
 }
